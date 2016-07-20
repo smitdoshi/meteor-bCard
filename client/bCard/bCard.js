20bf5a026eda27aa76f3e726b6fc763eb8b18169
SMIT N DOSHI <smitdoshi007@gmail.com>
2016-07-20 18:52:27 -0400

    added bCard.js

--- client/bCard/bCard.js
+++ client/bCard/bCard.js
@@ -2,13 +2,13 @@
 Template.bCard.helpers({
 	firstName: function(){
 		return Session.get('firstName');
-	}
+	},
 	lastName: function(){
 		return Session.get('lastName');
-	}
+	},
 	quote: function(){
 		return Session.get('quote');
-	}
+	},
 	githubLink: function(){
 		return Session.get('githubLink');
 	}
